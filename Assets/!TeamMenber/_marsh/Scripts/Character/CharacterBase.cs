dfa1309254d291bae734cb6c23c2baa8593e37cf
marsh <f.koji5654@gmail.com>
2025-07-07 11:47:03 +0900

    Playerの基盤作成

---
+++ Assets/!TeamMenber/_marsh/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//全キャラ共通のステータス
+
+public class CharacterBase{
+
+    public int hp;
+    public int maxHp;
+    public int attack;
+    public float stamina;
+    public float moveSpeed;
+    public float coolTime;
+
+
+}
---
+++ Assets/!TeamMenber/_marsh/Scripts/Character/Player/PlayerBase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Update ‚ÉƒvƒŒƒCƒ„[‚Ì‘€ì‚Ô‚¿‚Ş
+
+public class PlayerBase : CharacterBase {
+
+    void Update() {
+
+    }
+}
