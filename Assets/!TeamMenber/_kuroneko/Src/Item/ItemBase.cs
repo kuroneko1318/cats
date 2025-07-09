1d40f2bd5562b2da70acfa3d1250fd012556d123
kuroneko1318 <nekkokuro5@gmail.com>
2025-07-09 15:09:09 +0900

    Add ItemBase and ItemManager

---
+++ Assets/!TeamMenber/_kuroneko/Src/Item/ItemBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eItemType {
+    Weapon,     // 武器(1000~1999)
+    Armor,      // 防具(2000~2999)
+    Heal,       // 回復アイテム（3000~3999）
+    Material,   // 素材アイテム（4000~4999）
+    Money       // 換金アイテム・通貨など(5000~5999)
+}
+
+public class ItemBase
+{
+    public string itemName;    // アイテムの名前（表示名）
+    public int itemID;         // 一意のID（ここでは1〜5の連番）
+    public eItemType type;      // アイテムのカテゴリ（武器、防具など）
+
+    // コンストラクタ：新しいアイテムを作成
+    public ItemBase(string name, int id, eItemType type) {
+        this.itemName = name;
+        this.itemID = id;
+        this.type = type;
+    }
+}
