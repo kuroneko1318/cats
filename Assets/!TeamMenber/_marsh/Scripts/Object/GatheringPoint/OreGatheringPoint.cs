0df3db48d1a5cfae8e3c7b66e07109e56ce292af
marsh <f.koji5654@gmail.com>
2025-07-14 11:31:17 +0900

    add itemname
    採取ポイント仮実装

--- Assets/!TeamMenber/_marsh/Scripts/Object/GatheringPoint/OreGatheringPoint.cs
+++ Assets/!TeamMenber/_marsh/Scripts/Object/GatheringPoint/OreGatheringPoint.cs
@@ -15,7 +15,7 @@ public class OreGatheringPoint : GatheringPoint {
 
     protected override void Gather() {
         Debug.Log("zÎ‚©‚ç“SzÎ‚ğÌæ‚µ‚Ü‚µ‚½I");
-        bag.AddItem(instance.GetItemByID("");
+        bag.AddItem(instance.GetItemByName("ÎA"), 50);
     }
 
     private void Update() {
