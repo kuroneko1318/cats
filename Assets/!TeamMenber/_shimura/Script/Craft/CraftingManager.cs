a10da4c93c9f80b8ba4c4eedbe6ed72af2e9eb12
shimura0207 <mcd2476002@stu.o-hara.ac.jp>
2025-07-16 13:47:53 +0900

    Add Craft Complete

--- Assets/!TeamMenber/_shimura/Script/Craft/CraftingManager.cs
+++ Assets/!TeamMenber/_shimura/Script/Craft/CraftingManager.cs
@@ -9,31 +9,36 @@ public class CraftingManager:SystemObject<CraftingManager> {
     public ItemManager itemManager;
     
     private const int _POTION_ID = 4007;
-    
+
+    Inventory inventory = null;
     public void ItemInitialize() {
         itemManager.Initialize();
     }
 
     // 合成処理：2つのアイテムを合成して新しいアイテムを返す
-    public ItemBase CraftItem(string name1, string name2) {
-        
+    public void CraftItem(string name1, string name2) {
+
+        if (inventory == null) {
+            inventory = GameObject.FindGameObjectWithTag("Bag").GetComponent<Inventory>();
+        }
         ItemBase item1 = itemManager.GetItemByName(name1);
         ItemBase item2 = itemManager.GetItemByName(name2);
 
         if (item1 == null || item2 == null) {
             Debug.LogWarning("合成に失敗：アイテムが見つかりません");
-            return null;
+            return;
         }
 
         // 合成ルール（例：薬草 + 鉄 → 回復薬）
         if ((item1.itemName == "薬草" && item2.itemName == "鉄") ||
  (item1.itemName == "鉄" && item2.itemName == "薬草")) {
-            return ItemManager.Instance.GetItemByID(_POTION_ID);
+           inventory.AddItem(ItemManager.Instance.GetItemByID(_POTION_ID),1);
+            return;
         }
 
         // 他の合成ルールを追加可能
         Debug.Log("合成ルールに一致しません");
-        return null;
+        return;
     }
 
 }
