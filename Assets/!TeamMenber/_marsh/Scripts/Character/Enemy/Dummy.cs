50cb096cdc21de725d1d58cb467ba4c6d0895d36
marsh <f.koji5654@gmail.com>
2025-07-23 11:25:04 +0900

    fixed
    引数の入れ忘れの修正

--- Assets/!TeamMenber/_marsh/Scripts/Character/Enemy/Dummy.cs
+++ Assets/!TeamMenber/_marsh/Scripts/Character/Enemy/Dummy.cs
@@ -10,12 +10,7 @@ public class Dummy : EnemyBase {
 
     private void Update() {
         //デバッグでJキーでダメージ
-        if (Input.GetKeyDown(KeyCode.J)) TakeDamage();
-    }
-
-    private IEnumerator ResetHitFlagAfterDelay(float delay) {
-        yield return new WaitForSeconds(delay);
-        animator.SetBool("Hit", false);
+        if (Input.GetKeyDown(KeyCode.J)) TakeDamage(100, 1.1f, 0.3f, 2);
     }
 
 
