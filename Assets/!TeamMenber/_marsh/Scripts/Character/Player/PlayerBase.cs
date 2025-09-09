ac93804a260b79e760a910e07f13fa453256a065
shimura0207 <mcd2476002@stu.o-hara.ac.jp>
2025-09-09 09:48:44 +0900

    add collider

--- Assets/!TeamMenber/_marsh/Scripts/Character/Player/PlayerBase.cs
+++ Assets/!TeamMenber/_marsh/Scripts/Character/Player/PlayerBase.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 //Update にプレイヤーの操作ぶち込む
 
 public class PlayerBase : CharacterBase {
+
+    public PlayerInput input;
+    public InputAction moveAction;
+    public InputAction jumpAction;
+    public InputAction attackAction;
+    public InputAction avoidanceAction;
+    public InputAction GatherAction;
+    public GameObject cam;
+
+    public Rigidbody rb;
+    public Collider damageCollider;
+
+    public Vector3 latestPos;
+    public Vector3 direction;
+    public Vector3 diff;
+
+    public bool isAvoiding = false;
+    public static bool attackFlag = false;
+    private float avoidanceTimer = 0;
+    private float avoidanceDuration = 0.002f;
+
+    private float avoidanceCooldown = 1f;
+    private float cooldownTimer = 0f;
+
+    public Vector3 initialPosition;
 
     //会心率　最大値は1.00f
     public float criticalChance;
@@ -73,4 +99,109 @@ public class PlayerBase : CharacterBase {
         animator.SetBool("Hit", false);
     }
 
+
+
+    public void Death() {
+        if (hp <= 0) {
+            animator.SetTrigger("Death");
+        }
+    }
+
+    public void PlayerMove() {
+        if (isAvoiding) return;
+
+        Vector2 inputVector = moveAction.ReadValue<Vector2>();
+        if (inputVector != Vector2.zero) {
+            animator.SetBool("Run", true);
+
+            Vector3 camForward = cam.transform.forward;
+            Vector3 camRight = cam.transform.right;
+
+            camForward.y = 0;
+            camRight.y = 0;
+            camForward.Normalize();
+            camRight.Normalize();
+
+            Vector3 moveDir = camForward * inputVector.y + camRight * inputVector.x;
+
+            rb.velocity = moveDir * moveSpeed;
+            direction = moveDir;
+
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+        else {
+            animator.SetBool("Run", false);
+        }
+    }
+
+
+    public void RotMove() {
+        Vector2 inputVector = moveAction.ReadValue<Vector2>();
+        if (inputVector != Vector2.zero) {
+            Vector3 camForward = cam.transform.forward;
+            Vector3 camRight = cam.transform.right;
+
+            camForward.y = 0;
+            camRight.y = 0;
+            camForward.Normalize();
+            camRight.Normalize();
+
+            Vector3 moveDir = camForward * inputVector.y + camRight * inputVector.x;
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+    }
+
+    public void Avoidance() {
+        if (cooldownTimer > 0f) {
+            cooldownTimer -= Time.deltaTime;
+            return;
+        }
+
+        if (avoidanceAction.WasPressedThisFrame() && !isAvoiding) {
+            animator.SetTrigger("Avoidance");
+
+            isAvoiding = true;
+            isInvincible = true;
+            avoidanceTimer = avoidanceDuration;
+            cooldownTimer = avoidanceCooldown;
+
+            rb.velocity = transform.forward * moveSpeed * 2f;
+        }
+
+        if (isAvoiding) {
+            avoidanceTimer -= Time.deltaTime;
+            if (avoidanceTimer <= 0f) {
+                isAvoiding = false;
+                isInvincible = false;
+            }
+        }
+    }
+
+    public void Stop() {
+        if (!isAvoiding && !moveAction.IsInProgress() && !avoidanceAction.IsInProgress()) {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+
+    public void AddAttack(int _weaponATK) {
+        attack += _weaponATK;
+    }
+
+
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("GatheringPoint") && GatherAction.WasPressedThisFrame()) {
+            var point = other.gameObject.GetComponent<GatheringPoint>();
+            point?.Gather();
+        }
+    }
+
+    // 死亡アニメーション終了後に呼ばれる
+    public void OnDeathAnimationEnd() {
+        transform.position = initialPosition;
+        hp = maxHp;
+        animator.ResetTrigger("Death");
+        // 必要に応じて他の初期化処理
+    }
 }
