dd5a3d19bf12afa5485264713f537622ccecb0c2
shimura0207 <mcd2476002@stu.o-hara.ac.jp>
2025-09-29 12:18:36 +0900

    fixed showButton

--- Assets/!TeamMenber/_kuroneko/Src/Player/GaterCooldownUI.cs
+++ Assets/!TeamMenber/_kuroneko/Src/Player/GaterCooldownUI.cs
@@ -5,29 +5,13 @@ using UnityEngine.UI;
 /// スキルのクールタイムをUIスライダーで表示する
 /// </summary>
 public class GaterCooldownUI : MonoBehaviour {
-    [Header("UIスライダー")]
-    [SerializeField] private Slider cooldownSlider; // UIのスライダー
 
-    [Header("対象プレイヤー")]
-    //[SerializeField] private PlayerBase player;    // プレイヤー参照
-
-    [Header("対象スキル")]
-    //[SerializeField] private int skillIndex = 0;   // 表示するスキルのインデックス
 
     [Header("表示非表示用")]
     [SerializeField] private GameObject skillCooldownUI;
 
-
-    float max=15;
-    private void Update() {
-       
+    private void Start() {
         var gater=GetComponent<GatheringPoint>();
-
-        float remaining = gater.respawnTime;
-
-        // 0→1に正規化してスライダーに反映
-        float normalized = 1f - (remaining / max);
-        cooldownSlider.value = normalized;
     }
 
     public void ShowUI() {
