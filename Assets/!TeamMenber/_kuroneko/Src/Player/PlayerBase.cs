e5a32fa350d9c1f6956cfd9acd230aa3b9784caa
marsh <f.koji5654@gmail.com>
2025-09-29 12:00:11 +0900

    add se
    SE追加したけど聞こえない

--- Assets/!TeamMenber/_kuroneko/Src/Player/PlayerBase.cs
+++ Assets/!TeamMenber/_kuroneko/Src/Player/PlayerBase.cs
@@ -293,6 +293,7 @@ public class PlayerBase : MonoBehaviour {
         if (hp <= 0) {
             isDead = true;
             anim.SetTrigger("Death");
+            AudioManager.Instance.PlaySE("YOUDIED");
             GameManager.Instance.OnDead();
         }
     }
